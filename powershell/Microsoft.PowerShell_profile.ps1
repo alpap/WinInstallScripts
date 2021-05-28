e84ce2d8de803f295552a0105f4bef78a928b630
Alexandros Papadopoulos <al.pap@live.com>
2021-05-28 10:29:04 +0200

    added starship

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,5 @@ function sudo {
     [string]$argsString = $args -join " "
     Start-Process -Verb RunAs "C:\Program Files\PowerShell\7\pwsh.exe" -Args "-NoExit -executionpolicy bypass -command Set-Location \`"$PWD\`"; ${argsString}"
 }
+
+Invoke-Expression (&starship init powershell)
